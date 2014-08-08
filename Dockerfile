32fae2bedc3aba60377689f3cd4137431a09065a
Tim Dettrick <t.dettrick@uq.edu.au>
2014-08-08 00:19:58 +0000

    Working config.

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM centos:centos7
 MAINTAINER t.dettrick@uq.edu.au
 
 # Set defaults which should be overridden on run
-ENV CONFIG_DIR "/opt/config"
-ENV SSL_DIR "/opt/ssl"
-ENV DIT4C_DOMAIN "dit4c.metadata.net"
+ENV CONFIG_DIR /opt/config
+ENV SSL_DIR /opt/ssl
+ENV DIT4C_DOMAIN dit4c.metadata.net
 
 RUN yum install -y docker
 
--- opt/run.sh
+++ opt/run.sh
@@ -6,57 +6,53 @@ echo "All other config files should be in $CONFIG_DIR"
 
 DOCKER_SOCKET="/var/run/docker.sock"
 
-if [ !-S $DOCKER_SOCKET ]
+if [ ! -S $DOCKER_SOCKET ]
 then
     echo "Host Docker socket should be mounted at $DOCKER_SOCKET"
     exit 1
 fi
 
-docker run --name dit4c_ssl_keys -v $SSL_DIR:/etc/ssl busybox \
-  "stat /etc/ssl/server.key && stat /etc/ssl/server.crt"
+docker run --name dit4c_ssl_keys -v $SSL_DIR:/etc/ssl centos:centos7 \
+  stat /etc/ssl/server.key /etc/ssl/server.crt || exit 1
 
 KEYS_EXIST=!$?
 
-if [ !$KEYS_EXIST ]
+if [ ! $KEYS_EXIST ]
 then
     echo "Required keys are missing from $SSL_DIR"
     exit 1
 fi
 
 # Create DB servers
-docker run --name dit4c_couchdb \
-    -v /var/log/dit4c_couchdb:/var/log \
+docker run -d --name dit4c_couchdb \
+    -v /var/log/dit4c_couchdb:/var/log/couchdb \
     -v /var/lib/couchdb \
     fedora/couchdb
-docker run --name dit4c_redis \
-    -v /var/log/dit4c_redis:/var/log \
+docker run -d --name dit4c_redis \
+    -v /var/log/dit4c_redis:/var/log/redis \
     -v /var/lib/redis \
     fedora/redis
 
 # Create highcommand and hipache servers
-docker run --name dit4c_highcommand \
-    -v /var/log/dit4c_highcommand:/var/log \
-    -v $CONFIG_DIR/dit4c-highcommand.conf:/etc/dit4c-highcommand.conf
+docker run -d --name dit4c_highcommand \
+    -v /var/log/dit4c_highcommand/supervisor:/var/log/supervisor \
+    -v $CONFIG_DIR/dit4c-highcommand.conf:/etc/dit4c-highcommand.conf \
     --link dit4c_redis:redis \
     --link dit4c_couchdb:couchdb \
     dit4c/dit4c-platform-highcommand
-docker run --name dit4c_hipache \
+docker run -d --name dit4c_hipache \
     --link dit4c_redis:redis \
-    -v /var/log/dit4c_hipache:/var/log \
+    -v /var/log/dit4c_hipache/supervisor:/var/log/supervisor \
+    -v /var/log/dit4c_hipache/hipache_access.log:/var/log/hipache_access.log \
     dit4c/dit4c-platform-hipache
 
-docker run --name dit4c_ssl \
+docker run -d --name dit4c_ssl \
     -p 80:80 -p 443:443 \
     -e DIT4C_DOMAIN=$DIT4C_DOMAIN \
     --volumes-from dit4c_ssl_keys:ro \
     --link dit4c_highcommand:dit4c-highcommand \
     --link dit4c_hipache:hipache \
-    -v /var/log/dit4c_ssl:/var/log \
+    -v /var/log/dit4c_ssl/nginx:/var/log/nginx \
+    -v /var/log/dit4c_ssl/supervisor:/var/log/supervisor \
     dit4c/dit4c-platform-ssl
-
-
-
-
-
-
 
