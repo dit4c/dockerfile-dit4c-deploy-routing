b455ccb100959c610fb67c1ca5ad9f262213b6cb
Tim Dettrick <t.dettrick@uq.edu.au>
2015-09-17 15:57:09 +1000

    Updating for Switchboard

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 # DOCKER-VERSION 1.1.0
-FROM centos:centos7
+FROM alpine:3.2
 MAINTAINER t.dettrick@uq.edu.au
 
 # Set defaults which should be overridden on run
 ENV SSL_DIR /opt/ssl
 ENV DIT4C_DOMAIN dit4c.metadata.net
-ENV ETCD_VERSION v2.0.0
-ENV SERVICE_DISCOVERY_PATH /dit4c/containers
 
-RUN yum install -y curl docker
+RUN apk add --update curl docker && rm -rf /var/cache/apk/*
 
 ADD /opt /opt
 
-CMD ["bash", "/opt/run.sh"]
+CMD ["sh", "/opt/run.sh"]
--- opt/run.sh
+++ opt/run.sh
@@ -1,20 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 echo "Setting up routing frontend for $DIT4C_DOMAIN"
 echo "SSL key & certificate should be in $SSL_DIR"
 
 DOCKER_SOCKET="/var/run/docker.sock"
-ETCD_IMAGE="quay.io/coreos/etcd:$ETCD_VERSION"
 
 if [ ! -S $DOCKER_SOCKET ]
 then
     echo "Host Docker socket should be mounted at $DOCKER_SOCKET"
-    exit 1
-fi
-
-if [[ $HOST == "" ]]
-then
-    echo "HOST should be specified as an environment variable"
     exit 1
 fi
 
@@ -37,45 +32,13 @@ else
     exit 1
 fi
 
-# Establish if we need a local etcd, or we can reuse the host etcd
-HOST_IP=$(ip route|awk '/default/ { print $3 }')
-curl -XGET -H "Connection: close" -v "http://$HOST_IP:2379/v2/stats/self"
-if [[ $? == 0 ]]
-then
-  echo "Using host etcd"
-  docker start dit4c_etcd || docker run -d --name dit4c_etcd \
-    --restart=always \
-    --expose 2379 -e ETCD_PORT_2379_TCP="tcp://$HOST_IP:2379" \
-    svendowideit/ambassador
-else
-  echo "Using standalone etcd"
-  docker start dit4c_etcd || docker run -d --name dit4c_etcd \
-    -e ETCD_DATA_DIR=/var/lib/etcd \
-    -e ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379,http://0.0.0.0:4001" \
-    -v /var/lib/etcd \
-    --restart=always \
-    $ETCD_IMAGE
-fi
-
-# Wait a little to ensure dit4c_etcd exist
-until [[ `docker inspect dit4c_etcd; echo $?` ]]
-do
-    sleep 1
-done
-
-ETCD_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_etcd)
-ETCDCTL_CMD="docker run --rm -e ETCDCTL_PEERS=$ETCD_IP:2379 --entrypoint /etcdctl $ETCD_IMAGE --no-sync"
-
 # Create SSL termination frontend
-docker start dit4c_ssl || docker run -d --name dit4c_ssl \
-    -p 80:8080 -p 443:8443 \
+docker start dit4c_switchboard || docker run -d --name dit4c_switchboard \
+    -p 443:8080 \
     -e DIT4C_DOMAIN=$DIT4C_DOMAIN \
+    -e DIT4C_ROUTE_FEED=https://$DIT4C_DOMAIN/routes \
     --volumes-from dit4c_ssl_keys:ro \
-    --link dit4c_etcd:etcd \
     --restart=always \
-    dit4c/dit4c-platform-ssl
-$ETCDCTL_CMD set "$SERVICE_DISCOVERY_PATH/dit4c_ssl/$HOST" \
-  $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_ssl)
+    dit4c/dit4c-platform-switchboard
 
 echo "Done configuring routing."
-echo "Ensure you have a Hipache config for your portal."
